4b7c1c4299ded738ea789a9f2812a68a24eef3a7
Piotr Mitkowski <piotr.mitkowski@e-msi.pl>
2018-05-01 23:46:53 +0200

    Reinitialized project

---
+++ PlanningPoker2018-backend-2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlanningPoker2018_backend_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
