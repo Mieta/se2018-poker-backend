3008fabecdec58f645d0ebb730458eeedbc905e7
Piotr Mitkowski <piotr.mitkowski@e-msi.pl>
2018-06-04 23:52:01 +0200

    Added migration with RoomId in User model

---
+++ PlanningPoker2018-backend-2/Migrations/20180604214932_AddedRoomIdToUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PlanningPoker2018_backend_2.Migrations
+{
+    public partial class AddedRoomIdToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "roomId",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "ProjectTask",
+                nullable: true,
+                defaultValue: "Unestimated",
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "roomId",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "ProjectTask",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true,
+                oldDefaultValue: "Unestimated");
+        }
+    }
+}
