6a635acef5f49422a4c07568aa8538f30112b220
Piotr Mitkowski <piotr.mitkowski@e-msi.pl>
2018-06-13 16:12:53 +0200

    Added basic behavior test

---
+++ PlanningPoker2018_backend.Tests/ManageTasksFeature.cs
@@ -0,0 +1,30 @@
+﻿using PlanningPoker2018_backend_2.Entities;
+using PlanningPoker2018_backend_2.Models;
+using Xbehave;
+using Xunit;
+
+namespace PlanningPoker2018_backend.Tests
+{
+    public class ManageTasksFeature
+    {
+        [Scenario]
+        public void SelectingTaskToEstimate(ProjectTask task)
+        {
+            "Given a new task"
+                .x(() => task = new ProjectTask()
+                {
+                    id = 1,
+                    status = TaskStatus.UNESTIMATED.name,
+                    title = "New task in BDD",
+                    RoomId = 999,
+                    estimate = 0
+                });
+
+            "When I select task"
+                .x(() => task.status = TaskStatus.VOTING.name);
+
+            "Then task status is 'Voting'"
+                .x(() => Assert.Equal(task.status, TaskStatus.VOTING.name));
+        }
+    }
+}
