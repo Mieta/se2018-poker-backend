9a0ab6e53da6e32fb87c4d63cff9c6b1d0c04605
Piotr Mitkowski <piotr.mitkowski@e-msi.pl>
2018-06-17 01:13:56 +0200

    Fixed imports after merge

--- PlanningPoker2018-backend-2/Controllers/ProjectTasksController.cs
+++ PlanningPoker2018-backend-2/Controllers/ProjectTasksController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PlanningPoker2018_backend_2.Entities;
 using PlanningPoker2018_backend_2.Models;
