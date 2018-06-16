0eb63c5570d73049accc664667c5e1b3dde79879
Piotr Mitkowski <piotr.mitkowski@e-msi.pl>
2018-06-16 14:03:26 +0200

    Added context and controller references to Manage Tasks Feature tests

--- PlanningPoker2018_backend.Tests/ManageTasksFeature.cs
+++ PlanningPoker2018_backend.Tests/ManageTasksFeature.cs
@@ -1,4 +1,6 @@
-﻿using PlanningPoker2018_backend_2.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PlanningPoker2018_backend_2.Controllers;
+using PlanningPoker2018_backend_2.Entities;
 using PlanningPoker2018_backend_2.Models;
 using Xbehave;
 using Xunit;
@@ -7,6 +9,13 @@ namespace PlanningPoker2018_backend.Tests
 {
     public class ManageTasksFeature
     {
+        private DatabaseContext _context;
+
+        public ManageTasksFeature()
+        {
+            SetUpTest();
+        }
+
         [Scenario]
         public void SelectingTaskToEstimate(ProjectTask task)
         {
@@ -19,12 +28,27 @@ namespace PlanningPoker2018_backend.Tests
                     RoomId = 999,
                     estimate = 0
                 });
+            "And having the task in database"
+                .x(() =>
+                {
+                    _context.ProjectTask.Add(task);
+                    _context.SaveChanges();
+                });
 
             "When I select task"
-                .x(() => task.status = TaskStatus.VOTING.name);
+                .x(async () =>
+                {
+                    await new ProjectTasksController(_context).UpdateTaskStatus(task.id, TaskStatus.VOTING.name);
+                });
 
             "Then task status is 'Voting'"
                 .x(() => Assert.Equal(task.status, TaskStatus.VOTING.name));
+        }
+
+        private void SetUpTest()
+        {
+            var builder = new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("Planning-Poker-DB");
+            _context = new DatabaseContext(builder.Options);
         }
     }
 }
